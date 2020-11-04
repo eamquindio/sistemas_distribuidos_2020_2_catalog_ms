d30e6db9df4e1655bb2f6d727a41d4056ceb3810
sicg18 <37753311+sicg18@users.noreply.github.com>
2020-11-04 18:59:44 -0500

    ProductStoreDiscount - ProductStore

--- src/main/java/co/edu/eam/sistemasdistribuidos/catalog/services/product_stores_discountService.java
+++ src/main/java/co/edu/eam/sistemasdistribuidos/catalog/services/product_stores_discountService.java
@@ -6,6 +6,9 @@ import co.edu.eam.sistemasdistribuidos.catalog.models.product_stores_discounts;
 import co.edu.eam.sistemasdistribuidos.catalog.repositories.Product_stores_discountRepository;
 import co.edu.eam.sistemasdistribuidos.catalog.exceptions.BusinessException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -16,16 +19,19 @@ public class product_stores_discountService {
     @Autowired
     private Product_stores_discountRepository Product_stores_discountRepository;
 
-    public void create(product_stores_discounts p) {
+    @CachePut(value="product_store_discount", key = "#p.id")
+    public product_stores_discounts create(product_stores_discounts p) {
 
         boolean producExist = Product_stores_discountRepository.existsById(p.getId());
         if (producExist) throw  new BusinessException ("ya existe el descuento en el producto", "Product_Discount_Exists");
 
         //guardo el descuento del producto si no existe
         Product_stores_discountRepository.save(p);
+        return p;
     }
 
 
+    @Cacheable(value = "product_store_discount", key = "#id")
     public product_stores_discounts buscar(Integer id) {
 
         boolean producExist = Product_stores_discountRepository.existsById(id);
@@ -37,16 +43,18 @@ public class product_stores_discountService {
     }
 
 
-    public void update(Integer id, product_stores_discounts p) {
-
+    @CachePut(value="product_store_discount", key = "#id", unless="#result == null")
+    public product_stores_discounts update(Integer id, product_stores_discounts p) {
         boolean producExist = Product_stores_discountRepository.existsById(id);
         if (!producExist) throw  new NotFoundException("no existe el descuento en el producto", "Product_Discount_Not_Found");
         //guardo el id en el objecto producto y despues lo guardo o actualizo si existe
         p.setId(id);
         Product_stores_discountRepository.save(p);
+        return p;
     }
 
 
+    @CacheEvict(value="product_store_discount", key="#id")
     public void delete(Integer id){
 
         boolean producExist = Product_stores_discountRepository.existsById(id);
